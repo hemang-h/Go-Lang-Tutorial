4ff85a7c89fdbe037589eb50dcfc99400d658fb7
hemang-h <hemangcertificate@gmail.com>
2023-02-22 21:31:41 +0530

    👔 Passing Pointer parameter

--- 05Pointers/Advance.go
+++ 05Pointers/Advance.go
@@ -10,7 +10,13 @@ type Player struct {
 
 var healthy int 
 
-func takeDamageFromExplosion(player *Player) {
+func (player Player) takeDamageFromExplosion(intensity int) {                                 //We can declare the function like this
+	fmt.Println("Function before the number hit and enter a intensity")
+	player.health -= (intensity + 5)
+	fmt.Println("HEALTH in function is: ", player.health)
+}
+
+func takeDamageFromExplosion(player *Player) {                                               // player *Player shows that I am referencing the value in the memory 
 	fmt.Println("The player is about to take a damage, enter the intensity in numbers")
 	var intensity int
 	fmt.Scan(&intensity)
@@ -22,13 +28,14 @@ func takeDamageFromExplosion(player *Player) {
 func main() {
 	fmt.Println("Give a health to your player")
 	fmt.Scan(&healthy)
-	players := &Player {
+	players := &Player {                                                                       //Here I have initialized the player directly as a pointer
 		health: healthy,
 		Name: "Hemang",
 	}
 	fmt.Println("Health of the BEFORE explosion player is: ", players.health)
 	
-	takeDamageFromExplosion(players)
+	players.takeDamageFromExplosion(10)
+	takeDamageFromExplosion(players)                                                         // I can also pass the same value as takeDamageFromExplosion(&players) note the & here and compare with parameter passed in the function 
 	
 	fmt.Println("Health of the AFTER explosion player is: ", players.health)
 }
