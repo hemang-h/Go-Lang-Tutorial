7c3d4f15c7875470344ad7d288e127ff5247b7f7
hemang-h <hemangcertificate@gmail.com>
2023-02-22 19:57:20 +0530

    ⚡️ Added Pointer to the function

--- 05Pointers/Advance.go
+++ 05Pointers/Advance.go
@@ -4,23 +4,27 @@ import "fmt"
 
 type Player struct {
 	health int
+	Name string
 }
+
 
 var healthy int 
 
-func takeDamageFromExplosion(player Player) {
+func takeDamageFromExplosion(player *Player) {
 	fmt.Println("The player is about to take a damage, enter the intensity in numbers")
 	var intensity int
 	fmt.Scan(&intensity)
 	player.health -= intensity
 	fmt.Println("HEALTH in function is: ", player.health)
+	fmt.Println("NAME in function is: ", player.Name)
 }
 
 func main() {
 	fmt.Println("Give a health to your player")
 	fmt.Scan(&healthy)
-	players := Player {
+	players := &Player {
 		health: healthy,
+		Name: "Hemang",
 	}
 	fmt.Println("Health of the BEFORE explosion player is: ", players.health)
 	
