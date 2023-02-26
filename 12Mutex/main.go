4ba9558b42bd4820387620a9564b8e87b99961e2
hemang-h <hemangcertificate@gmail.com>
2023-02-26 16:33:51 +0530

    🔖 V1: Player damage without Goroutine

--- 12Mutex/main.go
+++ 12Mutex/main.go
@@ -1,11 +1,43 @@
-package main 
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
 
 type Player struct {
 	health int
 }
 
 func newPlayer() *Player {
+	
 	return &Player{
 		health: 100,
 	}
-} 
+}
+
+func (players *Player) startUILoop() {
+	ticker := time.NewTicker(time.Second)
+	i := 0
+	for {
+		players.health -= rand.Intn(40)
+		if players.health < 0 {
+			fmt.Printf("THE PLAYER IS DEAD AFTER %v ROUNDS!!! \n", i)
+			break
+		}
+		fmt.Printf("Player health is: %d \r", players.health)
+		<- ticker.C
+		i++
+	}
+}
+
+func main() {
+	// players := &Player {
+	// 	health: ,
+	// }
+	newPlayer().startUILoop()
+	
+
+}
+
