7cc46ebc87cbedec533d7fc78d2e42eb7305d074
hemang-h <hemangcertificate@gmail.com>
2023-02-26 17:38:40 +0530

    ♻️ MUTEX introduction

--- 12Mutex/main.go
+++ 12Mutex/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"math/rand"
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 type Player struct {
 	health int
+	mu sync.RWMutex
 }
 
 func newPlayer() *Player {
@@ -18,26 +22,51 @@ func newPlayer() *Player {
 }
 
 func (players *Player) startUILoop() {
+	// defer  wg.Done()
 	ticker := time.NewTicker(time.Second)
-	i := 0
 	for {
-		players.health -= rand.Intn(40)
+		players.mu.RLock()
+		fmt.Printf("Player health is: %v \n", players.health)
+		<- ticker.C
+		players.mu.RUnlock()
+	}
+
+}
+
+func (players *Player) getDamage() {
+	ticker := time.NewTicker(time.Millisecond * 300)
+	i := 0
+	rounds := []int{}
+	for {
+		
+		players.mu.Lock()
+		r:= rand.Intn(30)		
+		players.health -= r
+
+		println("R:", r)
+		rounds = append(rounds, r)
+
 		if players.health < 0 {
 			fmt.Printf("THE PLAYER IS DEAD AFTER %v ROUNDS!!! \n", i)
+			fmt.Println("The damage values are: ")
+			for _, value := range rounds {
+				fmt.Print(" ;", value)
+			}			
+
 			break
 		}
-		fmt.Printf("Player health is: %d \r", players.health)
+
+		players.mu.Unlock()
 		<- ticker.C
 		i++
 	}
 }
 
 func main() {
-	// players := &Player {
-	// 	health: ,
-	// }
-	newPlayer().startUILoop()
-	
-
+	player := newPlayer()	
+	// wg.Add(1)
+	go player.startUILoop()
+	player.getDamage()
+	// wg.Wait()
 }
 
